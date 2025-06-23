5d13e1f0a657f1411782e55118b49ec47f090b38
Zak Grivell <89150707+Zak0749@users.noreply.github.com>
2025-06-23 14:10:54 +0100

    Create zed.nix

---
+++ modules/home/zed.nix
@@ -0,0 +1,37 @@
+{}: {
+  home.file.".config/zed/settings.json".json = {
+    icon_theme = {
+      light = "Catppuccin Latte";
+      dark = "Catppuccin Frappé";
+      mode = "system";
+    };
+    theme = {
+      light = "Catppuccin Latte";
+      dark = "Catppuccin Frappé";
+      mode = "system";
+    };
+    vim_mode = true;
+    ui_font_size = 16;
+    buffer_font_size = 16;
+    vim = {
+      default_mode = "helix_normal";
+    };
+    toolbar = {
+      agent_review = false;
+      breadcrumbs = false;
+      quick_actions = false;
+      selections_menu = false;
+    };
+    auto_install_extensions = {
+      catppuccin = true;
+      catppuccin_icons = true;
+      nix = true;
+    };
+    auto_save = "on_focus_change";
+    tab_bar =  {
+      show = true;
+      show_nav_history_buttons = false;
+      show_tab_bar_buttons = false;
+    };
+  };
+}
