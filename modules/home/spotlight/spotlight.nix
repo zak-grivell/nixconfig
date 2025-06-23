3e53bdb455d24c47e90f0520786e9febc875308a
Zak Grivell <89150707+Zak0749@users.noreply.github.com>
2025-06-23 15:43:02 +0100

    Update spotlight.nix

--- modules/home/spotlight/spotlight.nix
+++ modules/home/spotlight/spotlight.nix
@@ -1,5 +1,5 @@
 { lib, ... }: {
-  home.activation.makeTrampolineApps = lib.hm.dag.entryAfter [ "writeBoundary" ] (
-    builtins.readFile ./make-app-trampolines.sh
-  );
+  # home.activation.makeTrampolineApps = lib.hm.dag.entryAfter [ "writeBoundary" ] (
+  #   builtins.readFile ./make-app-trampolines.sh
+  # );
 }
