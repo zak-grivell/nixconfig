ac2bc0e85127bf1f7404b4b66c8abf65c07cabd7
Zak Grivell <89150707+Zak0749@users.noreply.github.com>
2025-06-23 15:29:51 +0100

    Move make-app-trampolines.sh to parent directory

--- modules/home/spotlight/spotlight.nix
+++ modules/home/spotlight/spotlight.nix
@@ -1,5 +1,5 @@
 { lib, ... }: {
   home.activation.makeTrampolineApps = lib.hm.dag.entryAfter [ "writeBoundary" ] (
-    builtins.readFile ./make-app-trampolines.sh
+    builtins.readFile ../make-app-trampolines.sh
   );
 }
