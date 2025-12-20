18269e1ab5d894c42efdf2e38a9bba37940b350b
Zak Grivell <zak@grivell.uk>
2025-12-20 20:01:39 +0000

    4

--- modules/home/windowing/sketchybar/sketchybar.nix
+++ modules/home/windowing/sketchybar/sketchybar.nix
@@ -1,8 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  home.packages = [ pkgs.sbarlua ];
+
   programs.sketchybar = {
     enable = true;
-    config = ./config;
+    config = {
+      source = builtins.path {
+        path = ./config;
+        name = "sketchybar-config";
+      };
+      recursive = true;
+    };
+
+    sbarLuaPackage = pkgs.sbarlua;
 
     configType = "lua";
     extraLuaPackages = luaPkgs: with luaPkgs; [ cjson ];
